12e1112efb100e4063e5a4969fb37fe881568fbe
Dan Ross <dan@rosspixelworks.com>
2021-05-20 20:26:37 -0500

    Initial

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='drutils',
+    version='.1',
+    packages=['pdutils'],
+    url='',
+    license='MIT',
+    author='Dan Ross',
+    author_email='dan@rosspixelworks.com',
+    description='Dan\'s Utilities',
+    required=[
+        "pandas>=1.2"
+    ]
+)
